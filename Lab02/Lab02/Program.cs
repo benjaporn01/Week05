94088113a49be773266b2877fbdc46daba6b0983
benjaporn01 <59030179@kmitl.ac.th>
2017-09-09 12:24:28 +0700

    add multi 2

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp2
         static void Main()
         {
             for (int i = 1; i <= 12; i++)
-            {
+            {    
                 Console.WriteLine("{0, 2} x 2 = {1, 2}", i, i * 2);
             }
         }
