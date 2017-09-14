58eaa4fe5cdf4318bf2f81be9e86ec01999334fa
benjaporn01 <59030179@kmitl.ac.th>
2017-09-14 13:53:55 +0700

    add if else (a==2)

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -1,44 +1,19 @@
-﻿using System;
-
-namespace ConsoleApp2
+﻿
+using System;
+using System.IO;
+public class IfLearning
 {
-    class ForEachTest
+    public static void Main()
     {
-        static void Main(string[] args)
+        int a = 2;
+        if (a == 2)
         {
-            // Initial string array with month names.
-            string[] monthName = new string[] { "January","February",
-                         "March","April","May","June","July","August",
-                         "September","October","November","December"};
-
-            Console.WriteLine("-------- foreach,in loop");
-            foreach (string month in monthName)
-            {
-                System.Console.WriteLine(month);
-            }
-            System.Console.WriteLine();
-
-            // Compare the previous loop to a similar for loop.
-            Console.WriteLine("-------- for loop");
-            for (int i = 0; i < monthName.Length; i++)
-            {
-                System.Console.WriteLine(monthName[i]);
-            }
-            System.Console.WriteLine();
-
-
-            // You can maintain a count of the elements in the collection.
-            int count = 0;
-            foreach (string month in monthName)
-            {
-                count += 1;
-                System.Console.WriteLine("Element #{0}: {1}", count, month);
-            }
-            System.Console.WriteLine("Number of elements in the array: {0}", count);
-
-            // Keep the console open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            Console.WriteLine("execute this line");
         }
+        else
+        {
+            Console.WriteLine("execute another line too");
+        }
+        Console.WriteLine("this line is always execute");
     }
 }
